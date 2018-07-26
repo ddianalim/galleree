7c356c6c50277852e9abea9cac7aa3acc90e0fd7
dianalimnos <dianalim1117@gmail.com>
2018-07-26 13:48:28 -0700

    Got the individual views to work!!
    Only shows posts from your account!

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const auth = require('./helpers/auth');
 const Post = require('../models/post');
+const User = require('../models/user');
 
 const router = express.Router();
-
 
 // Posts index
 // router.get('/', auth.requireLogin, (req, res, next) => {
@@ -15,19 +15,37 @@ const router = express.Router();
 //     }
 //   });
 // });
-
+//
 router.get('/', auth.requireLogin, (req, res, next) => {
-  Post.find({users: res.locals.currentUserId}).sort({ date: -1}).exec(function(err, posts) {
-    if (err) {
+  Post.find({users: res.locals.currentUserId}, 'title', function(err, posts) {
+    if(err) {
       console.error(err);
+    } else {
+      res.render('posts/index', { posts: posts });
     }
-    res.render('posts/index', { posts: posts });
   });
 });
 
+// router.get('/', auth.requireLogin, (req, res, next) => {
+//   Post.find({users: res.locals.currentUserId}).exec(function(err, posts) {
+//     if (err) {
+//       console.error(err);
+//     }
+//     res.render('posts/index', { posts: posts });
+//   });
+// });
+
 // Posts new
-router.get('/new', auth.requireLogin, (req, res, next) => {
-  res.render('posts/new');
+// router.get('/new', auth.requireLogin, (req, res, next) => {
+//   res.render('posts/new');
+// });
+
+router.get('/new', auth.requireLogin, (req, res, next) =>{
+  User.findById(req.params.userId, function(err, post) { //TODO: User or Post??
+    if(err) { console.error(err);}
+
+    res.render('posts/new');
+  });
 });
 
 // Posts show
@@ -42,7 +60,7 @@ router.get('/:id', auth.requireLogin, (req, res, next) => {
 // Posts edit
 router.get('/:id/edit', auth.requireLogin, (req, res, next) => {
   Post.findById(req.params.id, function(err, post) {
-    if(err) { console.error(err) };
+    if (err) { console.error(err); }
 
     res.render('posts/edit', { post: post });
   });
@@ -52,7 +70,6 @@ router.get('/:id/edit', auth.requireLogin, (req, res, next) => {
 router.post('/:id', auth.requireLogin, (req, res, next) => {
   Post.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
     if(err) { console.error(err) };
-
     res.redirect('/posts/' + req.params.id);
   });
 });
@@ -61,11 +78,29 @@ router.post('/:id', auth.requireLogin, (req, res, next) => {
 router.post('/', auth.requireLogin, (req, res, next) => {
   let post = new Post(req.body);
 
+  post.users.push(req.session.userId);
+
   post.save(function(err, post) {
     if(err) { console.error(err) };
 
     return res.redirect('/posts');
   });
 });
+// router.post('/', auth.requireLogin, (req, res, next) => {
+//   User.findById(req.params.userId, function(err, user) {
+//     if(err) { console.error(err); }
+//
+//     let post = new Post(req.body);
+//     // post.user = user;
+//
+//     post.save(function(err, post) {
+//       if(err) { console.error(err) };
+//       // console.log(post.date)
+//       // return res.redirect(`/post/${post._id}`);
+//       return res.redirect('/posts');
+//     });
+//   });
+// });
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -14,19 +14,50 @@ router.get('/', auth.requireLogin, (req, res, next) => {
   });
 });
 
+// router.get('/', auth.requireLogin, (req, res, next) => {
+//   User.find({}, 'username', function(err, users) {
+//     if(err) {
+//       res.render('users/new');
+//     }
+//     res.render('users/index');
+//   });
+// });
+
 // Users new
 router.get('/new', (req, res, next) => {
   res.render('users/new');
 })
 
 // Users create
+// router.post('/', (req, res, next) => {
+//   const user = new User(req.body);
+//
+//   user.save(function(err, user) {
+//     if(err) console.log(err);
+//     return res.redirect('/users');
+//   });
+// });
+
 router.post('/', (req, res, next) => {
   const user = new User(req.body);
 
   user.save(function(err, user) {
-    if(err) console.log(err);
-    return res.redirect('/users');
+    if (err) {
+      console.log(err);
+    }
+    User.authenticate(req.body.username,
+    req.body.password, (err, user) => {
+      if (err || !user) {
+        const next_error = new Error("Username or password incorrect");
+        next_error.status = 401;
+
+        return next(next_error);
+      } else {
+        req.session.userId =  user._id;
+        return res.redirect('/');
+      }
+    });
   });
-})
+});
 
 module.exports = router;
